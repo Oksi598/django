db901fa8a9e6e3df6daff440ebc040f7015d0cc8
Oksi598 <radovunoksana@gmail.com>
2025-01-28 00:19:52 +0200

    views

---
+++ shop_project/shop/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for shop_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('category/<int:pk>/', views.category_detail, name='category_detail'),
+    path('product/add/', views.ProductCreateView.as_view(), name='product_add'),
+    path('product/<int:pk>/edit/', views.ProductUpdateView.as_view(), name='product_edit'),
+    path('product/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
+    path('register/', views.register, name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),]
+
--- shop_project/shop/views.py
+++ shop_project/shop/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+from .models import Product, Category
+
+def product_list(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'shop/product_list.html', {'products': products, 'categories': categories})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    return render(request, 'shop/product_detail.html', {'product': product})
+
+def category_detail(request, pk):
+    category = get_object_or_404(Category, id=pk)
+    products = Product.objects.filter(category=category)
+    return render(request, 'shop/category_detail.html', {'category': category, 'products': products})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'shop/register.html', {'form': form})
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name', 'description', 'price', 'category']
+    template_name = 'shop/product_form.html'
+    success_url = reverse_lazy('product_list')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'description', 'price', 'category']
+    template_name = 'shop/product_form.html'
+    success_url = reverse_lazy('product_list')
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'shop/product_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
+
+
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = ['name']
+    template_name = 'shop/category_form.html'
+    success_url = reverse_lazy('product_list')
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = ['name']
+    template_name = 'shop/category_form.html'
+    success_url = reverse_lazy('product_list')
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    template_name = 'shop/category_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
